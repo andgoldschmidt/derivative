04ca211d027e4bce485ff202a8cb6584a59c026f
Andy Goldschmidt <agolschmidt11@gmail.com>
2020-05-25 14:45:31 -0700

    v0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Andy Goldschmidt'
 author = 'Andy Goldschmidt'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
