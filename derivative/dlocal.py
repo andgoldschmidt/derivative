17cd88d1200f19e3b159a983c2844d2a6e95afce
andy <agoldschmidt11@gmail.com>
2020-09-16 20:57:11 -0700

    Add periodicity to Sovitzky-Golay derivatives

--- derivative/dlocal.py
+++ derivative/dlocal.py
@@ -10,14 +10,14 @@ class FiniteDifference(Derivative):
         Compute the symmetric numerical derivative of equally-spaced data using the Taylor series. Derivatives at the
         boundaries are computed with the available reduction of the window or first order finite difference.
 
-        A simple first order finite difference scheme in the spirit of this code is available in numpy.gradient.
+        A simple first order finite difference scheme is available in numpy.gradient.
 
         Args:
             k (int): Interpolate the data with a polynomial through the 2k+1 points x[i-k], ..., x[i], ... x[i+k]
             **kwargs: Optional keyword arguments.
 
         Keyword Args:
-            periodic (bool): If True, wrap the data. Assumes that x[-1 + 1] = x[0]. If False, compute edges separately.
+            periodic (bool): If True, wrap the data. Assumes that x[-1 + 1] = x[0]. If False, truncate at edges.
                 Default False.
         """
         self.k = k
@@ -60,7 +60,8 @@ class SavitzkyGolay(Derivative):
     def __init__(self, order, left, right, **kwargs):
         """ Compute the numerical derivative by first finding the best (least-squares) polynomial of order m < 2k+1
         using the 2k+1 points in the neighborhood [t-left, t+right]. The derivative is computed  from the coefficients
-        of the polynomial. The default edge behavior is to truncate the window at the offending edge.
+        of the polynomial. The default edge behavior is to truncate the window at the offending edge. The data does
+        not need to be sampled at equal timesteps.
 
         A simple symmetric version of the Savitzky-Galoy filter is available as scipy.signal.savgol_filter.
 
@@ -71,29 +72,56 @@ class SavitzkyGolay(Derivative):
             **kwargs: Optional keyword arguments.
 
         Keyword Args:
-            use_iwindow (bool): Whether to use an indexed window. If True, left and right act as indicies for t instead
+            iwindow (bool): Whether to use an indexed window. If True, left and right act as indicies for t instead
                 of as lengths in units of t. Default False.
+            periodic (bool): If True, wrap the data. Assumes that x[-1 + 1] = x[0]. If False, truncate at edges.
+                Default False.
+            T (float): If periodic is true, set T as the period of the data. Default is T = t[-1]-t[0] which is an
+                inacurate estimate.
+
         """
         self.left = left
         self.right = right
-        self.use_iwindow = kwargs.get('use_iwindow', False)
         self.order = order
+        self.iwindow = kwargs.get('iwindow', False)
+        self.periodic = kwargs.get('periodic', False)
+        self.T = kwargs.get('T', None)
 
-    def _trunc_window(self, t, i):
-        ileft = np.argmin(np.abs(t - (t[i] - self.left)))
-        iright = np.argmin(np.abs(t - (t[i] + self.right)))
-        return [ileft, iright]
+    def _nondimensional_window(self, t, i):
+        """ Create an indexed window from a dimensional window."""
+        if self.periodic:
+            tleft = t[i] - self.left
+            tright = t[i] + self.right
+            period = self.T if self.T else t[-1] - t[0]
+            # Find the nearest (signed) point and add attained periods
+            ileft = np.argmin((t - tleft) % period)
+            ileft = ileft - len(t)*np.abs(t[ileft]-tleft)//period
+            iright = np.argmin((t - tright) % period)
+            iright = iright + len(t)*np.abs(t[iright] - tright) // period
+        else:
+            # Find the nearest point
+            ileft = np.argmin(np.abs(t - (t[i] - self.left)))
+            iright = np.argmin(np.abs(t - (t[i] + self.right)))
+        return [int(ileft), int(iright)]
 
     def compute(self, t, x, i):
         # Default edge behavior is to truncate the window.
-        if self.use_iwindow:
-            i_l, i_r = max(0., i - self.left), min(i + self.right, len(t) - 1)
+        if self.iwindow:
+            i_l = int(i - self.left)
+            i_r = int(i + self.right)
+            if not self.periodic:
+                i_l = max(0, i_l)
+                i_r = min(i_r, len(t) - 1)
         else:
-            i_l, i_r = self._trunc_window(t, i)
+            i_l, i_r = self._nondimensional_window(t, i)
 
         # Construct a polynomial in t using least squares regression.
-        tfit = t[i_l:i_r]
-        xfit = x[i_l:i_r]
+        # Index views must allow for -left to +right, inclusively.
+        ii = np.arange(i_l, i_r + 1)
+        # Times are not periodic and initial values must be corrected.
+        period = t[-1]-t[0] if self.T is None else self.T
+        tfit = t[ii % len(t)] + period*(ii//len(t))
+        xfit = x[ii % len(t)]
         # Can raise RankWarning if order exceeds points in the window.
         w = polyfit(tfit, xfit, self.order)
 
